8a1e0f4fdd58ce6188fe125fef84d4194cf61f81
Yohann Bethoule <yohann.bethoule@gmail.com>
2024-01-26 12:02:19 +0100

    Add loader and reload on date change, move website select

--- src/components/WebsiteDashboard.vue
+++ src/components/WebsiteDashboard.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {AnalyticsData} from "../types/analytics.ts";
+import {AnalyticsData, WebsiteInfo} from "../types/analytics.ts";
 import MetricCard from "./MetricCard.vue";
 import eyeIcon from "@/assets/images/icons/eye.svg";
 import userIcon from "@/assets/images/icons/user.svg";
@@ -9,18 +9,32 @@ import deviceIcon from "@/assets/images/icons/device.svg";
 import MetricContent from "@/components/MetricContent.vue";
 import DonutChart from "@/components/DonutChart.vue";
 import {computed} from "vue";
+import BrutalDropdown from "@/components/BrutalDropdown.vue";
+import Loader from "@/components/Loader.vue";
 
 const props = defineProps<{
-    websiteAnalytics: AnalyticsData
+    analytics: AnalyticsData | undefined,
+    websites: WebsiteInfo[],
+    modelValue: string,
+    loading: boolean,
 }>()
 
+const emit = defineEmits<{
+    (event: 'update:modelValue', payload: string): void;
+}>()
+
+const handleWebsiteChange = (newValue: string) => {
+    emit('update:modelValue', newValue);
+};
+
 const deviceChartData = computed(() => {
+    if (!props.analytics) return {}
     return {
         labels: ['Desktop', 'Mobile'],
         datasets: [
             {
                 label: '%',
-                data: [props.websiteAnalytics.deviceType.desktop, props.websiteAnalytics.deviceType.mobile],
+                data: [props.analytics.deviceType.desktop, props.analytics.deviceType.mobile],
                 backgroundColor: [
                     '#FF6384',
                     '#36A2EB',
@@ -33,12 +47,13 @@ const deviceChartData = computed(() => {
 
 
 const originChartData = computed(() => {
+    if (!props.analytics) return {}
     return {
         labels: ['Direct', 'Internal', 'Referral', 'Search'],
         datasets: [
             {
                 label: '%',
-                data: [props.websiteAnalytics.sourceType.direct, props.websiteAnalytics.sourceType.internal, props.websiteAnalytics.sourceType.referral, props.websiteAnalytics.sourceType.search],
+                data: [props.analytics.sourceType.direct, props.analytics.sourceType.internal, props.analytics.sourceType.referral, props.analytics.sourceType.search],
                 backgroundColor: [
                     '#FF6384',
                     '#36A2EB',
@@ -53,27 +68,28 @@ const originChartData = computed(() => {
 </script>
 
 <template>
-    <section class="website-dashboard">
-        <h2>{{ websiteAnalytics.website.websiteName }}</h2>
+    <section class="website-dashboard" v-if="!loading && !!analytics">
+        <BrutalDropdown id="website-selector" v-if="websites?.length > 0" :model-value="modelValue"
+                        :options="websites.map(website => {return {label: website.websiteName, value: website.websiteID}})" @update:modelValue="handleWebsiteChange" />
         <div class="content">
             <div class="metrics numbers">
                 <MetricCard>
                     <template #icon>
                         <img :src="eyeIcon" class="icon" alt="eye icon"/>
                     </template>
-                    <MetricContent :title="websiteAnalytics.viewCount.toString()" label="vues"></MetricContent>
+                    <MetricContent :title="analytics.viewCount.toString()" label="vues"></MetricContent>
                 </MetricCard>
                 <MetricCard>
                     <template #icon>
                         <img :src="userIcon" class="icon" alt="user icon"/>
                     </template>
-                    <MetricContent :title="websiteAnalytics.sessionCount.toString()" label="sessions"></MetricContent>
+                    <MetricContent :title="analytics.sessionCount.toString()" label="sessions"></MetricContent>
                 </MetricCard>
                 <MetricCard>
                     <template #icon>
                         <img :src="timeIcon" class="icon" alt="time icon"/>
                     </template>
-                    <MetricContent :title="websiteAnalytics.sessionTime.mean + 'min'"
+                    <MetricContent :title="analytics.sessionTime.mean + 'min'"
                                    label="Temps de session"></MetricContent>
                 </MetricCard>
             </div>
@@ -95,7 +111,9 @@ const originChartData = computed(() => {
         </div>
 
     </section>
-
+    <section class="website-dashboard loader-container" v-else>
+        <Loader></Loader>
+    </section>
 </template>
 
 <style lang="scss" scoped>
@@ -118,6 +136,13 @@ const originChartData = computed(() => {
         border-radius: 0;
         height: fit-content;
     }
+}
+
+.loader-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
 }
 
 .content {
@@ -155,14 +180,19 @@ const originChartData = computed(() => {
     }
 }
 
-
-h2 {
-    color: #1a1a1a;
-}
-
 .icon {
     width: 4rem;
     height: 4rem;
     color: white;
+}
+
+#website-selector {
+    margin: 1rem auto 2rem 2rem;
+    padding: 1rem;
+    font-size: 2rem;
+
+    @media (max-width: $mobile-breakpoint) {
+        margin: 1rem auto 2rem auto;
+    }
 }
 </style>
