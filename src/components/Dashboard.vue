89b95801fe7058b6d52b25e13bec7e5a71d86fe2
Yohann Bethoule <yohann.bethoule@gmail.com>
2024-02-02 19:34:54 +0100

    Fix error handling

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -10,15 +10,24 @@ const selectedWebsiteId = ref("")
 const loading = ref(false)
 
 const selectedWebsiteAnalytics = computed(() => {
+    if (!userAnalytics.value.length) {
+        return
+    }
     return userAnalytics.value.find((analytics: AnalyticsData) => analytics.website.websiteID == selectedWebsiteId.value)
 })
 
 const websites = computed(() => {
+    if (!userAnalytics.value) {
+        return []
+    }
     return userAnalytics.value.map(website => website.website)
 })
 
 const handleAnalyticsUpdate = (analytics: AnalyticsData[]) => {
     userAnalytics.value = analytics
+    if (!userAnalytics.value.length) {
+        return
+    }
     selectedWebsiteId.value = analytics[0].website.websiteID
 }
 </script>
